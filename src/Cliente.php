2ae6953f0949df1a4e6908fd4c0b41ac782c10c1
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-21 14:56:28 -0300

    Adiciona método mostrarDados (provisório)

--- src/Cliente.php
+++ src/Cliente.php
@@ -5,4 +5,13 @@ class Cliente {
     public string $nome;
     public int $idade;
     public string $email;
+
+    /* Método responsável por exibir os dados de um objeto */
+    public function mostrarDados(){
+        echo "<div>
+                    <h4> NOME DO CLIENTE </h4>
+                    <p><b>E-mail de contato:</b> EMAIL DO CLIENTE </p>
+                    <p><b>Idade:</b> IDADE DO CLIENTE anos</p>
+                </div>";        
+    }
 }
