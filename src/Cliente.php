793565889ecd3f4c23ce63f397125e9d4527d98c
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-21 15:15:38 -0300

    Refatora método mostrarDados

--- src/Cliente.php
+++ src/Cliente.php
@@ -7,11 +7,13 @@ class Cliente {
     public string $email;
 
     /* Método responsável por exibir os dados de um objeto */
-    public function mostrarDados(){
+
+    // Usamos void para sinalizar que o método não tem return
+    public function mostrarDados():void {
         echo "<div>
-                    <h4> NOME DO CLIENTE </h4>
-                    <p><b>E-mail de contato:</b> EMAIL DO CLIENTE </p>
-                    <p><b>Idade:</b> IDADE DO CLIENTE anos</p>
+                    <h4> $this->nome </h4>
+                    <p><b>E-mail de contato:</b> $this->email </p>
+                    <p><b>Idade:</b> $this->idade anos</p>
                 </div>";        
     }
 }
