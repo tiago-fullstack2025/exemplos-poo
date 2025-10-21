39d450d0c9550353c21b1f45095e40d7b59e7284
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-21 17:22:53 -0300

    Método construtor (ainda incompleto)

--- src/Cliente.php
+++ src/Cliente.php
@@ -4,5 +4,12 @@ class Cliente {
     public int $idade;
     public string $email;
 
+    /* Método construtor: responsável por 'construir'
+    um objeto COM seus dados no momento da criação */
+    public function __construct( 
+        string $valorDoNome, int $valorDaIdade, string $valorDoEmail ) {
+        
+            
+    }
     
 }
