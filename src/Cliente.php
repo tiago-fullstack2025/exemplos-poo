d19c1b45b4f3eff53f213677a2ad92fc95ec10d6
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-21 17:17:35 -0300

    Reset

--- src/Cliente.php
+++ src/Cliente.php
@@ -1,19 +1,8 @@
 <?php
 class Cliente {
-    /* Atributos da classe: representam características
-    que os objetos deverão ter */
     public string $nome;
     public int $idade;
     public string $email;
 
-    /* Método responsável por exibir os dados de um objeto */
-
-    // Usamos void para sinalizar que o método não tem return
-    public function mostrarDados():void {
-        echo "<div>
-                    <h4> $this->nome </h4>
-                    <p><b>E-mail de contato:</b> $this->email </p>
-                    <p><b>Idade:</b> $this->idade anos</p>
-                </div>";        
-    }
+    
 }
