0b68ad7ab455b9acf30ced7d9c1a516fccd5fa13
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-22 16:21:19 -0300

    Adiciona método construtor

--- src/Livro.php
+++ src/Livro.php
@@ -2,5 +2,15 @@
 class Livro {
     public string $titulo;
     public string $autor;
-    public int $paginas;
+    public ?int $paginas;
+
+    public function __construct(
+        string $valorTitulo, 
+        string $valorAutor,
+        ?int $valorPaginas = null
+    ){
+        $this->titulo = $valorTitulo;
+        $this->autor = $valorAutor;
+        $this->paginas = $valorPaginas;
+    }
 }
