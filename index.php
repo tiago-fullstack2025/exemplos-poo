ce499af8bb3f4d5fef8bc341c18078a1ff5c6d5d
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-22 14:34:50 -0300

    Programa parâmetro opcional

--- index.php
+++ index.php
@@ -3,30 +3,25 @@ require_once "src/Cliente.php";
 
 // Criando um objeto e usando o método construtor para passar os dados
 $clienteA = new Cliente("Mônica", 30, "monica@gmail.com");
-
 $clienteB = new Cliente("José", 15, "jose@gmail.com");
 
+// Neste objeto, passamos inclusive o telefone (que nos anteriores, foi opcional)
+$clienteC = new Cliente("Joãozinho", 40, "joaozinho@gmail.com", "11-2135-0300");
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exemplos</title>
 </head>
-
 <body>
     <h1>Exemplos de PHP com POO</h1>
     <hr>
-    <h2>Trabalhando com classes e objetos</h2>
-
-    
-
-    <hr>
+    <h2>Trabalhando com classes e objetos</h2>   
     <h3>Visualizando a estrutura dos objetos</h3>
-    <pre><?=var_dump($clienteA, $clienteB)?></pre>
+    <pre><?=var_dump($clienteA, $clienteB, $clienteC)?></pre>
     
 </body>
 
--- src/Cliente.php
+++ src/Cliente.php
@@ -4,16 +4,24 @@ class Cliente {
     public int $idade;
     public string $email;
 
+    // Telefone é opcional, ou seja, caso não seja informado, ficará valendo null
+    public ?string $telefone; // ? indica que este atributo PODE ser NULL
+
     /* Método construtor: responsável por 'construir'
     um objeto COM seus dados no momento da criação */
     public function __construct( 
-        string $valorDoNome, int $valorDaIdade, string $valorDoEmail ) {
+        string $valorDoNome, 
+        int $valorDaIdade, 
+        string $valorDoEmail, 
+        ?string $valorDoTelefone = null // null como valor padrão
+    ) {
         
         /* Atribuindo os valores recebidos através dos parâmetros
         do construtor, aos atributos da classe */
         $this->nome = $valorDoNome;
         $this->idade = $valorDaIdade;
         $this->email = $valorDoEmail;
+        $this->telefone = $valorDoTelefone;
     }
     
 }
