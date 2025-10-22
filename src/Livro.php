0bc286ca6cc3eaecbd0d2ece61b4e75ee93d33cf
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-22 17:02:08 -0300

    Adiciona método do desafio

--- src/Livro.php
+++ src/Livro.php
@@ -13,4 +13,14 @@ class Livro {
         $this->autor = $valorAutor;
         $this->paginas = $valorPaginas;
     }
+
+    /* programe também (dentro da classe Livro) um método chamado verificarTitulo que, ao ser chamado, ele verifica se a quantidade de caracteres do atributo "titulo" é menor que 3. E se for, o método deve mostrar uma mensagem vermelha dizendo "Título não pode ter menos do que 3 letras */
+    public function verificarTitulo():void {
+
+        if( mb_strlen($this->titulo) < 3 ){
+            echo "<p style='color:red'>
+            Título não pode ter menos do que 3 letras</p>";
+        }
+
+    }
 }
